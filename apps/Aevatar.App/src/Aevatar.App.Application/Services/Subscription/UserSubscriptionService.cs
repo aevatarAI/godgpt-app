92f9b4e2975492eba0b18d69bbb9f73f16eb1e54
jason <jason.wang@aelf.io>
2026-02-03 00:22:43 +0800

    fix: add missing Extensions namespace import to subscription and payment services

--- apps/Aevatar.App/src/Aevatar.App.Application/Services/Subscription/UserSubscriptionService.cs
+++ apps/Aevatar.App/src/Aevatar.App.Application/Services/Subscription/UserSubscriptionService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Aevatar.Agents.Abstractions;
+using Aevatar.Agents.Abstractions.Extensions;
 using Aevatar.Agents.GodGPT.Protos.UserQuota;
 using Aevatar.App.Services.Subscription.Dtos;
 using Aevatar.App.Services.Subscription.Options;
